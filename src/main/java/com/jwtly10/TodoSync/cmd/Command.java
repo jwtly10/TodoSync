e283a7e52d29608adccef790679bbf555e3c25b5
Josh Watley <watleyjosh@gmail.com>
2023-12-10 15:07:38 +0000

    feat: Adding cmd & deps

---
+++ src/main/java/com/jwtly10/TodoSync/cmd/Command.java
@@ -0,0 +1,52 @@
+package com.jwtly10.TodoSync.cmd;
+
+import com.jwtly10.TodoSync.models.Todo;
+import com.jwtly10.TodoSync.parsers.DirectoryParser;
+import picocli.CommandLine;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+@CommandLine.Command(
+        name = "TodoSync",
+        description = "A command line tool to sync TODOs from your codebase to github issues",
+        mixinStandardHelpOptions = true,
+        version = "0.1.0")
+public class Command implements Runnable {
+
+    private final DirectoryParser directoryParser;
+
+    @CommandLine.Parameters(
+            index = "0",
+            description = "The directory to parse for TODOs",
+            defaultValue = ".")
+    private String directory;
+
+    @CommandLine.Option(
+            names = {"-v", "--verbose"}, description = "Enable verbose mode.")
+    private boolean verbose;
+
+    public Command(DirectoryParser directoryParser) {
+        this.directoryParser = directoryParser;
+    }
+
+    @Override
+    public void run() {
+        if (directory.equals(".")) {
+            directory = Paths.get("").toAbsolutePath().toString();
+            System.out.println("No directory specified, using current directory: " + directory);
+        }
+
+        System.out.println("Processing directory: " + directory);
+
+
+        if (verbose) {
+            System.out.println("Verbose mode enabled");
+        }
+
+        List<Todo> res = directoryParser.parseDir(directory);
+        for (Todo todo : res) {
+            System.out.println(todo.getTitle());
+        }
+    }
+}
